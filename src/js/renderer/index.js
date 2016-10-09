686115136eee95d3a17b9b0e09842bb98fa4abb6
Domikoapp <domikoapp@gmail.com>
2016-10-10 01:32:47 +0900

    ドロップされたファイルが画像であれば画像保存領域にコピーするよう変更

--- src/js/renderer/index.js
+++ src/js/renderer/index.js
@@ -1,14 +1,20 @@
 //ファイルドラッグ処理
+var filecopy = require("filecopy");
 document.getElementById("ddtarget").ondrop = function (e) {
     e.preventDefault();
     var files = e.dataTransfer.files;
+    // 画像情報をDBに登録し、画像保存領域に画像をコピー保存
+    var regex = /^image.+/g;
     for (var i = 0; i < files.length; i++) {
-        console.log("File : " + files[i].path);
-        var img = document.createElement("img");
-        img.src = files[i].path;
-        img.className = "thumbnail";
-        console.log(img.className);
-        document.body.appendChild(img);
+        var file = files[i];
+        if (!regex.test(file.type)) {
+            continue;
+        }
+        // DB登録
+        // ファイルコピー
+        filecopy(file.path, "db/pictures/" + file.name, {
+            mkdirp: true
+        });
     }
 };
 //# sourceMappingURL=index.js.map
--- src/ts/renderer/index.ts
+++ src/ts/renderer/index.ts
@@ -1,14 +1,23 @@
 //ファイルドラッグ処理
+const filecopy = require("filecopy");
+
 document.getElementById("ddtarget").ondrop = function(e) {
     e.preventDefault();
     let files:FileList = e.dataTransfer.files;
+    // 画像情報をDBに登録し、画像保存領域に画像をコピー保存
+    const regex = /^image.+/g;
     for (var i=0; i<files.length; i++) {
-        console.log("File : " + files[i].path);
-        var img = document.createElement("img");
-        img.src = files[i].path;
-        img.className = "thumbnail";
-        console.log(img.className);
-        document.body.appendChild(img);
+        let file:File = files[i];        
+        if (!regex.test(file.type)) {
+            continue;
+        }
 
+        // DB登録
+
+
+        // ファイルコピー
+        filecopy(file.path, "db/pictures/" + file.name, {
+             mkdirp: true
+        });
     }
 };
